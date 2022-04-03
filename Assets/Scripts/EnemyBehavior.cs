4d6ae925e800d39102a09b1603e62f0c61436471
ShadowCat567 <lv2067@nyu.edu>
2022-04-02 23:23:41 -0400

    Enemy functionality, damage

---
+++ Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    Rigidbody rb;
+    float enemyVelo = 3.0f;
+    public enum enemyType { x_dir, z_dir };
+    public enemyType typeOfEnemy;
+    Vector3 moveVect;
+
+    Renderer rend;
+    Color hitColor = new Color(0.86f, 0.02f, 0.02f);
+    float hitTimer = 0.4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rend = GetComponent<Renderer>();
+
+        switch(typeOfEnemy)
+        {
+            case enemyType.x_dir:
+                moveVect = new Vector3(1.0f, 0.0f, 0.0f);
+                break;
+            case enemyType.z_dir:
+                moveVect = new Vector3(0.0f, 0.0f, 1.0f);
+                break;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = moveVect * enemyVelo;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Wall")
+        {
+            enemyVelo = -enemyVelo;
+        }
+
+        if(collision.gameObject.tag == "Player")
+        {
+            StartCoroutine(changeColor(hitTimer));
+        }
+    }
+
+    IEnumerator changeColor(float showColor)
+    {
+        rend.material.color = hitColor;
+        yield return new WaitForSeconds(showColor);
+        rend.material.color = Color.white;
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,14 +5,24 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    Vector3 moveVector;
+    Vector3 moveVector = Vector3.zero;
     float velocity = 5.0f;
+    float jumpVelo = 2000.0f;
+    bool isJumping = false;
     Rigidbody rb;
+
+    int curHealth;
+    int maxHealth = 3;
+    Renderer rend;
+    Color dmgColor = new Color(0.98f, 0.0f, 0.31f);
+    float dmgColorTimer = 0.3f;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        rend = GetComponent<Renderer>();
+        curHealth = maxHealth;
     }
 
     public void OnMove(InputValue input)
@@ -22,13 +32,39 @@ public class PlayerMovement : MonoBehaviour
         moveVector = new Vector3(inputVector.x, 0, inputVector.y);
     }
 
+    /*
     public void OnJump()
     {
-
+        isJumping = true;
     }
+    */
 
     private void FixedUpdate()
     {
         rb.velocity = moveVector * velocity;
+
+        /*
+        if(isJumping)
+        {
+            rb.AddForce(Vector3.up * jumpVelo);
+            isJumping = false;
+        }
+        */
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            StartCoroutine(colorFlash(dmgColorTimer));
+            curHealth -= 1;
+        }
+    }
+
+    IEnumerator colorFlash(float flashTime)
+    {
+        rend.material.color = dmgColor;
+        yield return new WaitForSeconds(flashTime);
+        rend.material.color = Color.white;
     }
 }
