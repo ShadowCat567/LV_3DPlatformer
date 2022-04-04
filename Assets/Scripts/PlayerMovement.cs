a6d874fd3ca29fe1e79386e19a9b66ac4d7bde8f
ShadowCat567 <lv2067@nyu.edu>
2022-04-04 15:48:11 -0400

    Jump implemented

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
     Vector3 moveVector = Vector3.zero;
     float velocity = 5.0f;
-    float jumpVelo = 2000.0f;
     bool isJumping = false;
+    bool grounded = true;
     Rigidbody rb;
 
     int curHealth;
@@ -16,6 +17,8 @@ public class PlayerMovement : MonoBehaviour
     Renderer rend;
     Color dmgColor = new Color(0.98f, 0.0f, 0.31f);
     float dmgColorTimer = 0.3f;
+    [SerializeField] Transform respawnPos;
+    [SerializeField] Image[] healthArr = new Image[3];
 
     // Start is called before the first frame update
     void Start()
@@ -25,6 +28,24 @@ public class PlayerMovement : MonoBehaviour
         curHealth = maxHealth;
     }
 
+    private void Update()
+    {
+        if(curHealth == 0)
+        {
+            transform.position = respawnPos.position;
+            curHealth = maxHealth;
+            ResetHearts();
+        }
+    }
+
+    void ResetHearts()
+    {
+        for(int i = 0; i < healthArr.Length; i ++)
+        {
+            healthArr[i].enabled = true;
+        }
+    }
+
     public void OnMove(InputValue input)
     {
         Vector2 inputVector = input.Get<Vector2>();
@@ -32,24 +53,24 @@ public class PlayerMovement : MonoBehaviour
         moveVector = new Vector3(inputVector.x, 0, inputVector.y);
     }
 
-    /*
     public void OnJump()
     {
         isJumping = true;
     }
-    */
 
     private void FixedUpdate()
     {
         rb.velocity = moveVector * velocity;
 
-        /*
-        if(isJumping)
+        if (grounded)
         {
-            rb.AddForce(Vector3.up * jumpVelo);
-            isJumping = false;
+            if (isJumping)
+            {
+                rb.AddForce(new Vector3(0, 100, 0), ForceMode.Impulse);
+                isJumping = false;
+                grounded = false;
+            }
         }
-        */
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -58,6 +79,12 @@ public class PlayerMovement : MonoBehaviour
         {
             StartCoroutine(colorFlash(dmgColorTimer));
             curHealth -= 1;
+            healthArr[curHealth].enabled = false;
+        }
+
+        if(collision.gameObject.tag == "Ground")
+        {
+            grounded = true;
         }
     }
 
